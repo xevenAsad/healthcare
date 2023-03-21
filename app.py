5fbfdfda7422a4c0da1f7fb1d061076bf6b67c85
asad <asad@xevensolutions.com>
2023-03-21 17:40:20 +0500

    env configurations updated

--- app.py
+++ app.py
@@ -8,31 +8,35 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 
 
-
 class Database:
     def __init__(self):
-                # Load environment variables from .env file
+        # Load environment variables from .env file
         load_dotenv()
 
         # Get environment variables
-        sql_driver = os.environ.get('SQL_DRIVER')
-        sql_server = os.environ.get('SQL_SERVER')
-        sql_database = os.environ.get('SQL_DATABASE')
 
-        conn = pyodbc.connect(f"DRIVER={sql_driver};SERVER={sql_server};DATABASE={sql_database};")
+        sql_host = os.environ.get('SQL_HOST')
+        sql_UserId = os.environ.get('USERID')
+        sql_passowrd = os.environ.get('PASSWORD')
+        sql_database = os.environ.get('DATABASE')
+
+        conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+sql_host +
+                              ';DATABASE='+sql_database+';UID='+sql_UserId+';PWD=' + sql_passowrd)
 
     def insert_user_details(self, email, username, phone, company, total_blogs, remaining_blogs, active_status, profile_pic, description, Password):
         sql = "INSERT INTO UserDetails (Email, UserName, Phone, Company, TotalBlogs, RemainingBlogs, ActiveStatus, ProfilePic, Description, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
         cursor = self.conn.cursor()
-        cursor.execute(sql, (email, username, phone, company, total_blogs, remaining_blogs, active_status, profile_pic, description, Password))
+        cursor.execute(sql, (email, username, phone, company, total_blogs,
+                       remaining_blogs, active_status, profile_pic, description, Password))
         self.conn.commit()
 
     def insert_blog(self, title, description, created_by_id, request_id, is_active):
         sql = "INSERT INTO Blogs (Title, Description, CreatedById, RequestId, IsActive) VALUES (?, ?, ?, ?, ?)"
         cursor = self.conn.cursor()
-        cursor.execute(sql, (title, description, created_by_id, request_id, is_active))
+        cursor.execute(
+            sql, (title, description, created_by_id, request_id, is_active))
         self.conn.commit()
-        
+
     def get_user_details(self, email):
         cursor = self.conn.cursor()
         cursor.execute("SELECT * FROM UserDetails WHERE Email = ?", (email,))
@@ -55,24 +59,22 @@ class Database:
             return None
 
 
-
 class OpenAIAPI:
     def __init__(self):
-        openai.api_key =' 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'
+        openai.api_key = ' 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'
 
     def generate_blogpost(self, search_term):
 
-        
-            #preamble= f"Write an Article of approximately 500 words on '{search_term}' with in-text citations. Inclusion of in-text 'citations' of Latest Research is mandatory. Must Include a 'References' section at the end."
+        # preamble= f"Write an Article of approximately 500 words on '{search_term}' with in-text citations. Inclusion of in-text 'citations' of Latest Research is mandatory. Must Include a 'References' section at the end."
         prompt = f"Write an Article of approximately 500 words on '{search_term}' with in-text citations. Inclusion of in-text 'citations' of Latest Research is mandatory. Must Include a 'References' section at the end. Also give a catchy Title."
         try:
             response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
+                model="gpt-3.5-turbo",
+                messages=[
                     {"role": "system", "content": "You are a helpful assistant that writes blogposts on given topics."},
-                    {"role": "user", "content":prompt }
+                    {"role": "user", "content": prompt}
                 ],
-            temperature = 0.2
+                temperature=0.2
             )
         except Exception as e:
             print(f'Error : {str(e)}')
@@ -82,11 +84,13 @@ class OpenAIAPI:
         else:
             return None
 
+
 app = Flask(__name__)
 CORS(app)
 
 db = Database()
 openai_api = OpenAIAPI()
+
 
 @app.route('/users', methods=['POST'])
 def save_user():
@@ -101,24 +105,24 @@ def save_user():
     profile_pic = payload['ProfilePic']
     description = payload['Description']
     Password = payload['Password']
-    
 
     try:
         # Check if the email already exists in the database
         cursor = db.conn.cursor()
-        cursor.execute("SELECT Email FROM UserDetails WHERE Email = ?", (email,))
+        cursor.execute(
+            "SELECT Email FROM UserDetails WHERE Email = ?", (email,))
         existing_user = cursor.fetchone()
         if existing_user:
             return json.dumps({'status': 'error', 'message': 'User with this email already exists!'})
 
         # Insert the user details into the database
-        db.insert_user_details(email, username, phone, company, total_blogs, remaining_blogs, active_status, profile_pic, description,Password)
+        db.insert_user_details(email, username, phone, company, total_blogs,
+                               remaining_blogs, active_status, profile_pic, description, Password)
 
         return json.dumps({'status': 'success', 'User': payload})
     except Exception as e:
         print(str(e))
         return json.dumps({'status': 'error'})
-
 
 
 @app.route('/blogs', methods=['POST'])
@@ -139,18 +143,21 @@ def save_blogpost():
         description = openai_api.generate_blogpost(title)
 
         # Insert the blog post into the database
-        db.insert_blog(title, description, created_by_id, request_id, is_active)
+        db.insert_blog(title, description, created_by_id,
+                       request_id, is_active)
 
         return json.dumps({'status': 'success', 'BlogPost': description})
     except Exception as e:
         print(str(e))
         return json.dumps({'status': 'error'})
 
+
 @app.route('/GetAllUsers', methods=['GET'])
 def get_users():
     try:
         cursor = db.conn.cursor()
-        cursor.execute("SELECT Email, UserName, Phone, Company, TotalBlogs, RemainingBlogs, ActiveStatus FROM UserDetails")
+        cursor.execute(
+            "SELECT Email, UserName, Phone, Company, TotalBlogs, RemainingBlogs, ActiveStatus FROM UserDetails")
         rows = cursor.fetchall()
         users = []
         for row in rows:
@@ -168,7 +175,7 @@ def get_users():
     except Exception as e:
         print(str(e))
         return json.dumps({'status': 'error'})
-    
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -177,7 +184,8 @@ def login():
     password = payload['Password']
     try:
         cursor = db.conn.cursor()
-        cursor.execute("SELECT Email FROM UserDetails WHERE Email = ? AND Password = ?", (email, password))
+        cursor.execute(
+            "SELECT Email FROM UserDetails WHERE Email = ? AND Password = ?", (email, password))
         existing_user = cursor.fetchone()
         if existing_user:
             user = db.get_user_details(email)
@@ -189,19 +197,7 @@ def login():
         return json.dumps({'status': 'error'})
 
 
-
 if __name__ == '__main__':
     db = Database()
     openai_api = OpenAIAPI()
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
