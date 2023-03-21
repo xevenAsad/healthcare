1a8411ab27a090ede2e9319718e27417373cf790
asad <asad@xevensolutions.com>
2023-03-21 15:44:49 +0500

    initial files are committed

---
+++ AllApis.py
@@ -0,0 +1,178 @@
+import os
+import pyodbc
+from flask import Flask, request
+from Bio import Entrez
+import openai
+import json
+from flask_cors import CORS
+
+
+class Database:
+    def __init__(self):
+        self.conn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-839H9K1;DATABASE=healthcare_Dev;')
+
+    def insert_user_details(self, email, username, phone, company, total_blogs, remaining_blogs, active_status, profile_pic, description, Password):
+        sql = "INSERT INTO UserDetails (Email, UserName, Phone, Company, TotalBlogs, RemainingBlogs, ActiveStatus, ProfilePic, Description,Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        cursor = self.conn.cursor()
+        cursor.execute(sql, (email, username, phone, company, total_blogs, remaining_blogs, active_status, profile_pic, description,Password))
+        self.conn.commit()
+
+    def insert_blog(self, title, description, created_by_id, request_id, is_active):
+        sql = "INSERT INTO Blogs (Title, Description, CreatedById, RequestId, IsActive) VALUES (?, ?, ?, ?, ?)"
+        cursor = self.conn.cursor()
+        cursor.execute(sql, (title, description, created_by_id, request_id, is_active))
+        self.conn.commit()
+
+
+
+class OpenAIAPI:
+    def __init__(self):
+        openai.api_key =' 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'
+
+    def generate_blogpost(self, search_term):
+
+        
+            #preamble= f"Write an Article of approximately 500 words on '{search_term}' with in-text citations. Inclusion of in-text 'citations' of Latest Research is mandatory. Must Include a 'References' section at the end."
+        prompt = f"Write an Article of approximately 500 words on '{search_term}' with in-text citations. Inclusion of in-text 'citations' of Latest Research is mandatory. Must Include a 'References' section at the end. Also give a catchy Title."
+        try:
+            response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                    {"role": "system", "content": "You are a helpful assistant that writes blogposts on given topics."},
+                    {"role": "user", "content":prompt }
+                ],
+            temperature = 0.2
+            )
+        except Exception as e:
+            print(f'Error : {str(e)}')
+        blogpost = response['choices'][0]['message']['content']
+        if blogpost is not None:
+            return blogpost
+        else:
+            return None
+
+app = Flask(__name__)
+CORS(app)
+
+db = Database()
+openai_api = OpenAIAPI()
+
+@app.route('/users', methods=['POST'])
+def save_user():
+    payload = request.get_json()
+    email = payload['Email']
+    username = payload['UserName']
+    phone = payload['Phone']
+    company = payload['Company']
+    total_blogs = payload['TotalBlogs']
+    remaining_blogs = payload['RemainingBlogs']
+    active_status = payload['ActiveStatus']
+    profile_pic = payload['ProfilePic']
+    description = payload['Description']
+    Password = payload['Password']
+
+
+    try:
+        # Check if the email already exists in the database
+        cursor = db.conn.cursor()
+        cursor.execute("SELECT Email FROM UserDetails WHERE Email = ?", (email,))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            return json.dumps({'status': 'error', 'message': 'User with this email already exists!'})
+
+        # Insert the user details into the database
+        db.insert_user_details(email, username, phone, company, total_blogs, remaining_blogs, active_status, profile_pic, description,Password)
+
+        return json.dumps({'status': 'success', 'User': payload})
+    except Exception as e:
+        print(str(e))
+        return json.dumps({'status': 'error'})
+
+
+
+@app.route('/blogs', methods=['POST'])
+def save_blogpost():
+    payload = request.get_json()
+    title = payload['Title']
+    created_by_id = payload['CreatedById']
+    request_id = payload['RequestId']
+    is_active = payload['IsActive']
+
+    try:
+
+        print(title)
+        description = openai_api.generate_blogpost(title)
+        db.insert_blog(title, description, created_by_id, request_id, is_active)
+
+        return json.dumps({'status': 'success', 'BlogPost': description})
+    except Exception as e:
+        print(str(e))
+        return json.dumps({'status': 'error'})
+
+@app.route('/GetAllUsers', methods=['GET'])
+def get_users():
+    try:
+        cursor = db.conn.cursor()
+        cursor.execute("SELECT Email, UserName, Phone, Company, TotalBlogs, RemainingBlogs, ActiveStatus FROM UserDetails")
+        rows = cursor.fetchall()
+        users = []
+        for row in rows:
+            user = {'Email': row[0], 'UserName': row[1], 'Phone': row[2], 'Company': row[3], 'TotalBlogs': row[4], 'RemainingBlogs': row[5], 'ActiveStatus': row[6]}
+            users.append(user)
+        response = {'status': 'success', 'Users': users}
+        return json.dumps(response)
+    except Exception as e:
+        print(str(e))
+        response = {'status': 'error'}
+        return json.dumps(response)
+    
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        payload = request.get_json()
+        email = payload['Email']
+        password = payload['Password']
+        print(email,password)
+        cursor = db.conn.cursor()
+        cursor.execute("SELECT Email FROM UserDetails WHERE Email = ? AND Password = ?", (email, password))
+        user = cursor.fetchone()
+        if user:
+            response = {'status': 'success', 'message': 'User authenticated successfully.'}
+            return json.dumps(response)
+        else:
+            response = {'status': 'error', 'message': 'Invalid email or password.'}
+            return json.dumps(response)
+    except Exception as e:
+        print(str(e))
+        response = {'status': 'error', 'message': 'An error occurred during authentication.'}
+        return json.dumps(response)
+
+
+@app.route('/GetBlogsById/<int:Request_By_Id>', methods=['GET'])
+def get_blogposts(Request_By_Id):
+    try:
+        cursor = db.conn.cursor()
+        cursor.execute("SELECT Description, BlogId FROM Blogs WHERE RequestId = ?", (Request_By_Id,))
+        rows = cursor.fetchall()
+        if rows:
+            blog_posts = [{'BlogId': row[1], 'Description': row[0]} for row in rows]
+            response = {'status': 'success', 'blog_posts': blog_posts}
+            return json.dumps(response)
+        else:
+            response = {'status': 'error', 'message': 'No blog posts found for the specified user.'}
+            return json.dumps(response)
+    except Exception as e:
+        print(str(e))
+        response = {'status': 'error', 'message': 'An error occurred while retrieving the blog posts.'}
+        return json.dumps(response)
+
+
+
+if __name__ == '__main__':
+    db = Database()
+    openai_api = OpenAIAPI()
+    app.run(debug=True)
+
+
